8a17af0a5d1cefb9ebbf11714b7c6a5286f7a2a7
johnptmcdonald <johnptmcdonald@gmail.com>
2020-01-16 06:52:42 -0500

    adds app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,47 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  length = 0;
   password = "";
+  checkboxes = {
+    letters: false,
+    numbers: false,
+    symbols: false
+  };
+
+  onChangeLength(value: string) {
+    const parsedValue = +value;
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeCheckbox(event) {
+    this.checkboxes[event.target.name] = !this.checkboxes[event.target.name];
+  }
 
   onButtonClick() {
-    this.password = "thing";
+    const numbers = "123456789";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()";
+
+    let validChars = "";
+    if (this.checkboxes.letters) {
+      validChars += letters;
+    }
+    if (this.checkboxes.numbers) {
+      validChars += numbers;
+    }
+    if (this.checkboxes.symbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = "";
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
